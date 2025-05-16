ea238e989c3c5f49d3e78fa559a4c9c509b3868d
Aleksey Khlopkov <v.v.sidorovgg@gmail.com>
2025-05-16 19:35:31 +0300

    db initialization

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,11 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        org.example.db.DatabaseManager.connectAndInitialize();
+
         Application app = new Application();
         app.run();
+
+        
     }
 }
--- src/main/java/org/example/db/DatabaseManager.java
+++ src/main/java/org/example/db/DatabaseManager.java
@@ -1,4 +1,31 @@
 package org.example.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DatabaseManager {
+    private static final String URL = "jdbc:sqlite:projects.db";
+    private static Connection conn;
+    public static void connectAndInitialize() {
+        try {
+            conn = DriverManager.getConnection(URL);
+            initializeDB();
+        } catch (SQLException e) {
+            System.err.println("Error while trying to connect to the projects database: " + e.getMessage());
+        }
+    }
+    public static void initializeDB() {
+        String sql = "CREATE TABLE IF NOT EXISTS projects ("
+                + "id INTEGER PRIMARY KEY AUTOINCREMENT)";
+
+        try (Statement stmt = conn.createStatement()) {
+            stmt.execute(sql);
+
+        } catch (SQLException e) {
+            System.err.println("Error while trying to initialize the projects database: " + e.getMessage());
+            System.exit(1);
+        }
+    }
 }
